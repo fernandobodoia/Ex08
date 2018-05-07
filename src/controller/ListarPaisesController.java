69668e9b3e24af813fcae49008d65ca3e9cc4d4b
Fernando <Fernando@DESKTOP-ED09KFR>
2018-05-06 23:09:40 -0300

    ex08

---
+++ src/controller/ListarPaisesController.java
@@ -0,0 +1,45 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import Service.PaisService;
+import Model.Pais;
+
+@WebServlet("/listar_paises.do")
+public class ListarPaisesController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String chave = request.getParameter("data[search]");
+		String acao = request.getParameter("acao");
+		PaisService paisServ = new PaisService();
+		ArrayList<Pais> lista = null;
+		HttpSession session = request.getSession();
+		if (acao.equals("buscar")) {
+			if (chave != null && chave.length() > 0) {
+				lista = paisServ.listarPaises(chave);
+			} else {
+				lista = paisServ.listarPaises();
+			}
+			session.setAttribute("lista", lista);
+		} else if (acao.equals("reiniciar")) {
+			session.setAttribute("lista", null);
+		}
+		RequestDispatcher dispatcher = request.getRequestDispatcher("ListarPaises.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
